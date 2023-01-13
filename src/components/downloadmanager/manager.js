0d0387caaf1f7b61c5796eb49d1751059ab2d3a3
Shulaev Ivan <mail@ntdservice.ru>
2023-01-13 20:18:53 +0300

    clear code

---
+++ src/components/downloadmanager/manager.js
@@ -0,0 +1,66 @@
+import file1 from '../../assets/file1.pdf';
+import file2 from '../../assets/file2.pdf';
+import file3 from '../../assets/file3.pdf';
+import file4 from '../../assets/file4.pdf';
+
+export default class DownloadManager {
+  constructor(appTag) {
+    this.appContainer = document.querySelector(appTag);
+    this.fileRow = Array.from(this.appContainer.querySelectorAll('.position-row'));
+    this.allLinks = Array.from(this.appContainer.querySelectorAll('.file-link'));
+    this.allFiles = {
+      file1, file2, file3, file4,
+    };
+    this.allFilesParam = [];
+    this.totalTag = this.appContainer.querySelector('.size');
+    this.totalSum = 0;
+
+    this.onprogress = (e) => {
+      this.totalSum += e.loaded / 1024 / 1024;
+    };
+
+    this.download = (e) => {
+      const link = e.target.closest('.file-link');
+      const name = e.target.parentNode.parentNode.querySelector('.position-name');
+      const hrefData = e.target.href;
+      const file = new File([hrefData], '');
+      const reader = new FileReader();
+      reader.onprogress = this.onprogress;
+      reader.readAsDataURL(file);
+      link.setAttribute('download', name.textContent);
+      if (this.totalSum === 0) this.totalSum += Number(file.size / 1024 / 1024);
+      this.totalTag.textContent = `${this.totalSum.toFixed(2)}Mb`;
+      return false;
+    };
+  }
+
+  init() {
+    let counter = 1;
+    Object.entries(this.allFiles).forEach((item) => {
+      const file = new File([item], item[0]);
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      const fileParam = {
+        name: file.name,
+        size: `${(file.size / 1024 / 1024).toFixed(1)}Mb`,
+      };
+      this.allFilesParam.push(fileParam);
+    });
+
+    for (let i = 0; i < this.fileRow.length; i += 1) {
+      const positionSize = this.fileRow[i].querySelector('.position-size');
+      this.allLinks[i].addEventListener('click', this.download);
+      positionSize.textContent = this.allFilesParam[i].size;
+    }
+
+    this.allLinks.forEach((item) => {
+      if (counter < Object.keys(this.allFiles).length + 1) {
+        item.setAttribute('data-link', `file${counter}`);
+        // eslint-disable-next-line
+        item.href = this.allFiles[item.dataset.link];
+        counter += 1;
+        item.addEventListener('click', this.download);
+      }
+    });
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,6 @@
-import file1 from '../assets/file1.pdf';
-import file2 from '../assets/file2.pdf';
-import file3 from '../assets/file3.pdf';
-import file4 from '../assets/file4.pdf';
+import DownloadManager from '../components/downloadmanager/manager';
 
-class DownloadManager {
-    constructor(appTag) {
-        this.appContainer = document.querySelector(appTag);
-        this.fileRow = Array.from(this.appContainer.querySelectorAll('.position-row'));
-        this.allLinks = Array.from(this.appContainer.querySelectorAll('.file-link'));
-        this.allFiles = { file1, file2, file3, file4 };
-        this.allFilesParam = [];
-        this.total = this.appContainer.querySelector('.size');
-
-        this.download = (e) => {
-            const link = e.target.closest('.file-link')
-            const name = e.target.parentNode.parentNode.querySelector('.position-name');
-            const hrefData = e.target.href;
-            const file = new File([hrefData], '');
-            const fileSize = (file.size /1024 / 1024).toFixed(1)
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            link.setAttribute('download', name.textContent);
-            this.total.textContent.replace(' ', '');
-            console.log(this.total.textContent.replace('Mb', ''));
-            this.total.textContent = `${Number(this.total.textContent) + Number(fileSize) } Mb`;
-
-
-            return false;
-        }
-    }
-
-    addFiles() {
-        let counter = 1;
-        Object.entries(this.allFiles).forEach((item) => {
-            const file = new File([item], item[0]);
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            const fileParam = {
-                name: file.name,
-                size: (file.size / 1024 / 1024).toFixed(1) + 'Mb',
-            }
-            this.allFilesParam.push(fileParam)
-        });
-
-        for (let i = 0; i < this.fileRow.length; i++) {
-            let positionSize = this.fileRow[i].querySelector('.position-size');
-            this.allLinks[i].addEventListener('click', this.download);
-            positionSize.textContent = this.allFilesParam[i]['size'];
-        }
-
-        this.allLinks.forEach((item) => {
-            if (counter < Object.keys(this.allFiles).length + 1) {
-                item.setAttribute('data-link', `file${counter}`);
-                item.href = this.allFiles[item.dataset.link];
-                counter += 1;
-                item.addEventListener('click', this.download)
-            }
-        });
-    }
-}
 window.addEventListener('DOMContentLoaded', (e) => {
-    const downloadManager = new DownloadManager('.app-container')
-    downloadManager.addFiles()
-})
+  const downloadManager = new DownloadManager('.app-container');
+  downloadManager.init();
+});
